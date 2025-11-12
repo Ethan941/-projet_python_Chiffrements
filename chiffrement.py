9060d4c3f13df5fd4582c2dc85fb8da0963e1782
Ethan Pandor <ethanpandor43@gmail.com>
2025-11-12 09:59:29 +0100

    first commit: ajout du fichier chiffrement.py

---
+++ chiffrement.py
@@ -0,0 +1,9 @@
+def chiffre_cesar(text, key):#deffiniton de la fonction text + la clé
+    crypted_text =''#ont crypt le text
+
+    for char in text:#char élément de la liste  text =la liste général
+        crypted_text += chr(ord(char) + key)#ord = le nuémero de la place dans la liste
+
+    return crypted_text
+
+print(chiffre_cesar("ethan", 4))
